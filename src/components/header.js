beb5411b3d40955790b90aeed634d3899d72f07a
WhyNotSantosh <san_sriramadasu@yahoo.com>
2023-02-18 22:51:27 +0530

    init tailwind

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,7 @@ const Header = () => {
     return (
         <div className="header">
             <a href="/">
-                <img className="logo" alt="logo" src="https://i.pinimg.com/474x/e6/17/f1/e617f1bfb9af4d9cf132cd3dec0da072.jpg" />
+                <img className="h-28" alt="logo" src="https://i.pinimg.com/474x/e6/17/f1/e617f1bfb9af4d9cf132cd3dec0da072.jpg" />
             </a>
             <h1> Food Villa </h1>
             <ul className="nav-items">
