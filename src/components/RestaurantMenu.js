33b5b5d201e3e6a81136c34e941f5d92a0262544
WhyNotSantosh <san_sriramadasu@yahoo.com>
2023-03-22 12:08:11 +0530

    updated menu url

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -14,22 +14,22 @@ const RestaurantMenu = () => {
     return (
         <div>
             <h1> Restaurant Menu : {id} </h1>
-            <h2>{restaurant.name}</h2>
-            <img src={IMAGE_DOMAIN + restaurant.cloudinaryImageId} alt="item" />
-            <h3>{restaurant.area}</h3>
-            <h3>{restaurant.city}</h3>
-            <h3>{restaurant.avgRating} stars</h3>
-            <h3>{restaurant.costForTwoMsg}</h3>
+            <h2>{restaurant.cards[0].card.card.info.name}</h2>
+            <img src={IMAGE_DOMAIN + restaurant.cards[0].card.card.info.cloudinaryImageId} alt="item" />
+            <h3>{restaurant.cards[0].card.card.info.areaName}</h3>
+            <h3>{restaurant.cards[0].card.card.info.city}</h3>
+            <h3>{restaurant.cards[0].card.card.info.avgRating} stars</h3>
+            <h3>{restaurant.cards[0].card.card.info.costForTwoMessage}</h3>
             <div>
                 <h1>Menu</h1>
-                <ul>
+                {/* <ul>
                     {
                         Object.values(restaurant?.menu?.items).map((item, index) => {
                             return <li key={index}>{item.name}</li>
                         })
                     }
                     <li></li>
-                </ul>
+                </ul> */}
             </div>
         </div>
     )
