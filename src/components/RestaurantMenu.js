04b5d819ee406040d7f2268a978b4751ac563228
WhyNotSantosh <san_sriramadasu@yahoo.com>
2023-03-24 11:33:13 +0530

    updated footer and menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -10,6 +10,7 @@ const RestaurantMenu = () => {
     const { id } = useParams(); // read dynamic url params    
 
     const restaurant = useRestaurantMenu(id);
+    const restaurantInfo = restaurant?.cards[0]?.card?.card?.info;
     const dispatch = useDispatch();
     const handleAddItem = (item) => {
         dispatch(addItem(item));
@@ -23,23 +24,29 @@ const RestaurantMenu = () => {
         return <Shimmer />;
     }
     return (
-        <div>
-            <h1> Restaurant Menu : {id} </h1>
-            <h2>{restaurant.cards[0].card.card.info.name}</h2>
-            <img src={IMAGE_DOMAIN + restaurant.cards[0].card.card.info.cloudinaryImageId} alt="item" />
-            <h3>{restaurant.cards[0].card.card.info.areaName}</h3>
-            <h3>{restaurant.cards[0].card.card.info.city}</h3>
-            <h3>{restaurant.cards[0].card.card.info.avgRating} stars</h3>
-            <h3>{restaurant.cards[0].card.card.info.costForTwoMessage}</h3>
-            <div>
+        <div className="flex justify-center p-4">
+            {restaurantInfo && <div className="grid grid-cols-2 gap-12 border-dotted border-b-2 border-gray-600 p-2">
+                <div className="">
+                    <h1 className="font-bold p-2">{restaurantInfo.name}</h1>
+                    {/* <img src={IMAGE_DOMAIN + restaurant.cards[0].card.card.info.cloudinaryImageId} alt="item" /> */}
+                    <span className="p-2">{restaurantInfo.cuisines?.join(", ")}</span>
+                    <span className="p-2 flex">{restaurantInfo.areaName}, {restaurantInfo.feeDetails?.fees[0]?.fee / 1000}kms</span>
+                    <span className="p-2">{restaurantInfo.expectationNotifiers[0]?.text}</span>
+                </div>
+                <div className="grid grid-cols-1 justify-self-end border border-gray-300 rounded-lg text-center align-middle">
+                    <span className="p-2 border-b m-2 border-gray-300 font-bold text-green-700">{restaurantInfo.avgRating} stars</span>
+                    <span className="p-2 font-bold">{restaurantInfo.totalRatingsString}</span>
+                </div>
+            </div>}
+            {/* <div>
 
                 <button className="p-2 m-2 bg-green-400" onClick={() => {
                     handleRemoveItem()
                 }}>Remove Item</button>
             </div>
-            <div>
+             <div>
                 <h1>Menu</h1>
-                {/* <ul>
+                 <ul>
                     {
                         Object.values(restaurant?.menu?.items).map((item, index) => {
                             return <li key={index}>{item.name} -
@@ -49,9 +56,8 @@ const RestaurantMenu = () => {
                             </li>
                         })
                     }
-                </ul> */}
-            </div>
-
+                </ul> 
+            </div>*/}
         </div>
     )
 }
