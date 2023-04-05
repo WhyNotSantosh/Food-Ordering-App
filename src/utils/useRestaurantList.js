e3dbb9d064e6b1c44f0b486feef1d5182ebd593f
WhyNotSantosh <san_sriramadasu@yahoo.com>
2023-04-05 15:49:24 +0530

    Cors issue fixed

--- src/utils/useRestaurantList.js
+++ src/utils/useRestaurantList.js
@@ -2,24 +2,15 @@ import { useState, useEffect } from "react";
 import { SWIGGY_PUBLIC_DATA_END_POINT } from "../../constants";
 
 const useRestaurantList = () => {
-  const [restaurantsListData, setRestaurantsList] = useState();
-  const [searchRestaurantList, setSearchRestaurantList] = useState();
+  const [restaurantsListData, setRestaurantsList] = useState([]);
+  const [searchRestaurantList, setSearchRestaurantList] = useState([]);
   useEffect(() => {
     getRestaurantsListData();
   }, []);
 
   const getRestaurantsListData = async () => {
-    //Todo: CORS 
-    // try {
-    //   const cors = await fetch("https://cors-anywhere.herokuapp.com/corsdemo");
-    //   console.log(cors);
-    // } catch (e) {}
-
     try {
       const data = await fetch(SWIGGY_PUBLIC_DATA_END_POINT);
-      if (!data.ok) {
-        throw new Error(`Error! status: ${response.status}`);
-      }
       const json = await data.json();
       setRestaurantsList(json?.data?.cards[2]?.data?.data?.cards);
       setSearchRestaurantList(json?.data?.cards[2]?.data?.data?.cards);
