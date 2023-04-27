03b839a6eba7b27ac0038e9f656bcd91e42359d8
Santosh Kumar S R <105787626+WhyNotSantosh@users.noreply.github.com>
2023-04-27 16:43:04 +0530

    Update RestaurantCard.js
    updated optional chaining

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,10 +3,10 @@ import { IMAGE_DOMAIN } from "../../constants";
 const RestaurantCard = (props) => {
     return (
         <div className="w-64 rounded my-2">
-            <img src={IMAGE_DOMAIN + props.restaurantsList.data.cloudinaryImageId} />
-            <span className="font-bold font-sans pt-3 block">{props.restaurantsList.data.name}</span>
-            <span className="font-medium text-sm text-slate-500 font-sans mb-3 border-b pb-1 block">{props.restaurantsList.data.cuisines.join(', ')}</span>
-            <span className="font-medium text-sm text-slate-500 mb-3 block">{props.restaurantsList.data.lastMileTravelString.toUpperCase()}</span>
+            <img src={IMAGE_DOMAIN + props?.restaurantsList?.data?.cloudinaryImageId} />
+            <span className="font-bold font-sans pt-3 block">{props?.restaurantsList?.data?.name}</span>
+            <span className="font-medium text-sm text-slate-500 font-sans mb-3 border-b pb-1 block">{props?.restaurantsList?.data?.cuisines?.join(', ')}</span>
+            <span className="font-medium text-sm text-slate-500 mb-3 block">{props?.restaurantsList?.data?.lastMileTravelString?.toUpperCase()}</span>
         </div>
     )
 }
